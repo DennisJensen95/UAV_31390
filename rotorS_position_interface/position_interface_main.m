469d730cb40480f4bca30a0f4bd67a0719bb1b69
theismunk <s164022@student.dtu.dk>
2020-06-18 14:49:20 +0200

    maze 2d ex 6.8

--- rotorS_position_interface/position_interface_main.m
+++ rotorS_position_interface/position_interface_main.m
@@ -4,9 +4,9 @@
 % drone
 
 %%
-clc;
-close all;
-clear;
+clc
+close all
+clear
 %%
 
 % Define constants
@@ -19,3 +19,14 @@ GAZEBO_SYNC_DT =    0.01;
 RAD2DEG =           180/pi;
 DEG2RAD =           pi/180;
 THRUST_OFFSET =     15;
+
+%% PATH PLANNING
+addpath('../maze/');
+maze_1;
+route_start = [0, 0];
+route_end = [3, 6];
+
+route_2d = Astar_2d(map, route_start+1, route_end+1);
+height = ones(length(route_2d), 1);
+route_2d = [ route_2d height ]';
+route_2d = route_2d - [1;1;0];
